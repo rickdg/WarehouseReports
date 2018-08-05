97a0ab5216c24fb083b61ca850f5af1d0528b015
Mnham <yurik19rus@gmail.com>
2018-08-05 22:44:41 +0700

    LoadTasks

--- WarehouseReports.DAL/DataLoader/DataLoader.vb
+++ WarehouseReports.DAL/DataLoader/DataLoader.vb
@@ -1,38 +1,97 @@
-﻿Imports System.Data.SqlClient
+﻿Imports System.Data.OleDb
+Imports System.Data.SqlClient
 
 Public Class DataLoader
 
     Public Shared Function LoadTasks(fileName As String) As Boolean
-        Dim Excel = New ExcelTable(fileName)
-        Dim Sql = "SELECT MIN(F10), F8, F5
-                   FROM [{0}]
-                   WHERE F1 = 'Независимая задача' AND F10 IS NOT NULL
-                   GROUP BY F2, F3, F4, F5, F6, F7, F8, F9
-                   UNION ALL
-                   SELECT MIN(F10), F8, F5
-                   FROM [{0}]
-                   WHERE F1 = 'Дочерняя задача' AND F10 IS NOT NULL
-                   GROUP BY F4, F5, F6, F7, F8, F9"
-
-        If Not Excel.Fill(0, Sql) Then
-            Return False
-        End If
-
         Try
+            Dim ExcelTable As New DataTable
+
+            Using Connection As New OleDbConnection($"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={fileName};Extended Properties='Excel 12.0;HDR=NO';")
+                Connection.Open()
+                Dim Schema = (From Row In Connection.GetSchema("Columns")
+                              Group Row.Field(Of String)("COLUMN_NAME")
+                                  By TableName = Row.Field(Of String)("TABLE_NAME").Trim("'"c) Into Columns = ToList
+                              Where TableName.EndsWith("$")).ToList
+
+                Dim Table = Schema.ElementAt(0).TableName
+                Dim SQL As String = ""
+                Dim RecordCount As Integer
+
+                Select Case Schema.ElementAt(0).Columns.Count
+                    Case 3 ' Загрузка в док
+                        SQL = $"SELECT 6 AS SystemTaskType_id,
+                                       900 AS ZoneShipper,
+                                       900 AS ZoneConsignee,
+                                       'L900' AS UserTaskType,
+                                       F1 AS Employee,
+                                       F3 AS LoadTime
+                                FROM [{Table}]"
+                    Case 4 ' Получение
+                        SQL = $"SELECT 1 AS SystemTaskType_id,
+                                       NULL AS ZoneShipper,
+                                       0 AS ZoneConsignee,
+                                       'A000' AS UserTaskType,
+                                       F2 AS Employee,
+                                       MIN(F4) AS LoadTime
+                                FROM [{Table}]
+                                WHERE F1 = 'Получить' AND F3 IS NOT NULL
+                                GROUP BY F2, F3"
+                    Case 17
+                        SQL = $"SELECT 5 AS SystemTaskType_id,
+                                       F6 AS ZoneShipper,
+                                       F8 AS ZoneConsignee,
+                                       F10 AS UserTaskType,
+                                       F11 AS Employee,
+                                       MIN(F13) AS LoadTime
+                                FROM [{Table}]
+                                WHERE F2 = 'Отбор' AND F1 = 'Независимая задача'
+                                GROUP BY F3, F4, F5, F6, F7, F8, F9, F10, F11, F12
+
+                                UNION ALL
+
+                                SELECT 5 AS SystemTaskType_id,
+                                       F6 AS ZoneShipper,
+                                       F8 AS ZoneConsignee,
+                                       F10 AS UserTaskType,
+                                       F11 AS Employee,
+                                       MIN(F13) AS LoadTime
+                                FROM [{Table}]
+                                WHERE F2 = 'Отбор' AND F1 = 'Дочерняя задача'
+                                GROUP BY F5, F6, F7, F8, F9, F10, F11, F12
+
+                                UNION ALL
+
+                                SELECT 2 AS SystemTaskType_id,
+                                       IIF(F6 IS NULL, 0, F6) AS ZoneShipper,
+                                       F8 AS ZoneConsignee,
+                                       'W'&ZoneShipper&'C'&F8 AS UserTaskType,
+                                       F11 AS Employee,
+                                       MIN(F13) AS LoadTime
+                                FROM [{Table}]
+                                WHERE F2 = 'Размещение' AND F11 IS NOT NULL
+                                GROUP BY F6, F8, F11, F14, F15"
+                End Select
+
+                Using Adapter As New OleDbDataAdapter(SQL, Connection)
+                    RecordCount = Adapter.Fill(ExcelTable)
+                End Using
+
+                If RecordCount = 0 Then Return False
+            End Using
+
             Using Connection As New SqlConnection(My.Settings.WarehouseDataConnectionString)
                 Connection.Open()
                 Using Command = Connection.CreateCommand()
-                    Dim CommandText = "dbo.LoadTasks"
-                    Dim Parameter = "@ExcelTasks"
-                    Dim TypeName = "TaskExcelTable"
                     Command.CommandTimeout = 1800
-                    Command.CommandText = CommandText
+                    Command.CommandText = "dbo.LoadTasks"
                     Command.CommandType = CommandType.StoredProcedure
-                    Command.Parameters.Add(Parameter, SqlDbType.Structured).TypeName = TypeName
-                    Command.Parameters(Parameter).Value = Excel
+                    Command.Parameters.Add("@ExcelTasks", SqlDbType.Structured).TypeName = "TaskExcelTable"
+                    Command.Parameters("@ExcelTasks").Value = ExcelTable
                     Command.ExecuteReader()
                 End Using
             End Using
+
             MsgBox("Выполнено", MsgBoxStyle.Information)
             Return True
         Catch ex As Exception
@@ -40,4 +99,5 @@ Public Class DataLoader
             Return False
         End Try
     End Function
+
 End Class
