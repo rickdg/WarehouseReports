9938441db03e81b9792f4207286e288d1cdbadec
Mnham <yurik19rus@gmail.com>
2018-08-06 18:33:58 +0700

    UNION Контроль места консолидации

--- WarehouseReports.DAL/DataLoader/DataLoader.vb
+++ WarehouseReports.DAL/DataLoader/DataLoader.vb
@@ -70,7 +70,23 @@ Public Class DataLoader
                                        MIN(F13) AS LoadTime
                                 FROM [{Table}]
                                 WHERE F2 = 'Размещение' AND F11 IS NOT NULL
-                                GROUP BY F6, F8, F11, F14, F15"
+                                GROUP BY F6, F8, F11, F14, F15
+                                
+                                UNION ALL
+                                
+                                SELECT 7 AS SystemTaskType_id,
+                                       Move.ZoneShipper,
+                                       Move.ZoneConsignee,
+                                       'C900' AS UserTaskType,
+                                       Move.Employee,
+                                       MIN(Move.LoadTime)
+                                FROM (  SELECT F9 AS AddressConsignee, F16 AS LoadedLPN, F17 AS UnloadedLPN
+                                        FROM [{Table}]) Pick,
+                                     (  SELECT F6 AS ZoneShipper, F7 AS AddressShipper, F8 AS ZoneConsignee, F11 AS Employee, F12 AS LoadTime, F14 AS ContentLPN
+                                        FROM [{Table}]
+                                        WHERE F2 = 'Перемещение для промежуточного хранения' AND F6 = F8 AND F7 <> F9 AND F14 IS NOT NULL) Move
+                                WHERE Pick.UnloadedLPN = Move.ContentLPN AND Pick.AddressConsignee = Move.AddressShipper
+                                GROUP BY Pick.LoadedLPN, Move.ZoneShipper, Move.ZoneConsignee, Move.Employee"
                 End Select
 
                 Using Adapter As New OleDbDataAdapter(SQL, Connection)
