bcae61af9a2b9b6d589c8b24d45530a130abce53
Mnham <yurik19rus@gmail.com>
2018-08-07 16:46:00 +0700

    Убрана проверка на соответствие зоны отправителя и зоны получателя

--- WarehouseReports.DAL/DataBase/Контроль места консолидации.sql
+++ WarehouseReports.DAL/DataBase/Контроль места консолидации.sql
@@ -3,6 +3,6 @@ FROM (  SELECT F9 AS AddressConsignee, F16 AS LoadedLPN, F17 AS UnloadedLPN
         FROM [{Table}]) Pick,
      (  SELECT F6 AS ZoneShipper, F7 AS AddressShipper, F8 AS ZoneConsignee, F11 AS Employee, F12 AS LoadTime, F14 AS ContentLPN
         FROM [{Table}]
-        WHERE F2 = 'Перемещение для промежуточного хранения' AND F6 = F8 AND F7 <> F9 AND F14 IS NOT NULL) Move
+        WHERE F2 = 'Перемещение для промежуточного хранения' AND F7 <> F9 AND F14 IS NOT NULL) Move
 WHERE Pick.UnloadedLPN = Move.ContentLPN AND Pick.AddressConsignee = Move.AddressShipper
 GROUP BY Pick.LoadedLPN, Move.ZoneShipper, Move.ZoneConsignee, Move.Employee
