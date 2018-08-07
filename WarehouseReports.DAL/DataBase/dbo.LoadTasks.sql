9fcdeeb41701c77a3ededaf7bf6c83479e0cb5b5
Mnham <yurik19rus@gmail.com>
2018-08-07 16:42:49 +0700

    Убрана проверка вхождения задачи в диапазон времени загруженных задач

--- WarehouseReports.DAL/DataBase/dbo.LoadTasks.sql
+++ WarehouseReports.DAL/DataBase/dbo.LoadTasks.sql
@@ -8,8 +8,6 @@ AS
 			@Employee			NVARCHAR(MAX),
 			@LoadTime			DATETIME2(7),
 
-			@MinDate			DATETIME2(7),
-			@MaxDate			DATETIME2(7),
 			@Tmp_id				INT,
 			@Employee_id		INT,
 			@WeekdayNum			INT,
@@ -19,14 +17,6 @@ AS
 			@Norm				FLOAT(53)
 
 	SET DATEFIRST 1
-			
-	SELECT @MinDate = MIN(LoadTime), @MaxDate = MAX(LoadTime) FROM TaskData
-
-	IF @MinDate IS NULL
-		BEGIN
-			SET @MinDate = GETDATE()
-			SET @MaxDate = GETDATE()
-		END
 
 	DECLARE TableCursor CURSOR FOR SELECT SystemTaskType_id, ZoneShipper, ZoneConsignee, UserTaskType, Employee, LoadTime FROM @ExcelTasks
 
@@ -35,47 +25,45 @@ AS
 
 	WHILE @@FETCH_STATUS = 0
 		BEGIN
-			IF @LoadTime NOT BETWEEN @MinDate AND @MaxDate
+			SET @Tmp_id = (SELECT Id FROM Employee WHERE [Name] = @Employee)
+			IF @Tmp_id IS NULL
 				BEGIN
-					SET @Tmp_id = (SELECT Id FROM Employee WHERE [Name] = @Employee)
-					IF @Tmp_id IS NULL
-						BEGIN
-							INSERT INTO Employee([Name]) VALUES (@Employee)
-							SET @Employee_id = @@IDENTITY
-						END
-					ELSE
-						SET @Employee_id = @Tmp_id
-
-					SET @WeekdayNum = DATEPART(weekday, @LoadTime)
-
-					SET @Time = CONVERT(TIME(0), @LoadTime)
-					SELECT @GangNum = Number, @GangName = [Name] FROM Gang WHERE @Time BETWEEN StartTime AND EndTime
-
-					SET @Norm = (SELECT Norm FROM UserTaskType WHERE [Name] = @UserTaskType)
-					IF @Norm IS NULL
-						SET @Norm = 0
-
-					INSERT INTO TaskData(SystemTaskType_id, ZoneShipper, ZoneConsignee, UserTaskType, Employee_id, LoadTime, Norm,
-					YearNum,
-					MonthNum,
-					[MonthName],
-					WeekNum,
-					DayNum,
-					WeekdayNum,
-					WeekdayName,
-					HourNum,
-					GangNum, GangName)
-					VALUES (@SystemTaskType_id, @ZoneShipper, @ZoneConsignee, @UserTaskType, @Employee_id, @LoadTime, @Norm,
-					YEAR(@LoadTime),
-					MONTH(@LoadTime),
-					FORMAT(@LoadTime, 'MMMM', 'ru-RU'),
-					DATEPART(week, @LoadTime),
-					DAY(@LoadTime),
-					@WeekdayNum,
-					(SELECT [Name] FROM WeekdayName Where Id = @WeekdayNum),
-					DATEPART(hour, @LoadTime),
-					@GangNum, @GangName)
+					INSERT INTO Employee([Name]) VALUES (@Employee)
+					SET @Employee_id = @@IDENTITY
 				END
+			ELSE
+				SET @Employee_id = @Tmp_id
+
+			SET @WeekdayNum = DATEPART(weekday, @LoadTime)
+
+			SET @Time = CONVERT(TIME(0), @LoadTime)
+			SELECT @GangNum = Number, @GangName = [Name] FROM Gang WHERE @Time BETWEEN StartTime AND EndTime
+
+			SET @Norm = (SELECT Norm FROM UserTaskType WHERE [Name] = @UserTaskType)
+			IF @Norm IS NULL
+				SET @Norm = 0
+
+			INSERT INTO TaskData(SystemTaskType_id, ZoneShipper, ZoneConsignee, UserTaskType, Employee_id, LoadTime, Norm,
+			YearNum,
+			MonthNum,
+			[MonthName],
+			WeekNum,
+			DayNum,
+			WeekdayNum,
+			WeekdayName,
+			HourNum,
+			GangNum, GangName)
+			VALUES (@SystemTaskType_id, @ZoneShipper, @ZoneConsignee, @UserTaskType, @Employee_id, @LoadTime, @Norm,
+			YEAR(@LoadTime),
+			MONTH(@LoadTime),
+			FORMAT(@LoadTime, 'MMMM', 'ru-RU'),
+			DATEPART(week, @LoadTime),
+			DAY(@LoadTime),
+			@WeekdayNum,
+			(SELECT [Name] FROM WeekdayName Where Id = @WeekdayNum),
+			DATEPART(hour, @LoadTime),
+			@GangNum, @GangName)
+
 			FETCH NEXT FROM TableCursor INTO @SystemTaskType_id, @ZoneShipper, @ZoneConsignee, @UserTaskType, @Employee, @LoadTime
 		END
 	CLOSE TableCursor
