7d73a2a5309487ecc38afb658745fc54c1f2137c
Mnham <yurik19rus@gmail.com>
2018-08-07 12:18:51 +0700

    Доработан шаблон ExpressionNode

--- WarehouseReports/VM/TreeVM/Enums/LogicOperators.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class StringEnums
-
-    Public Function GetLogicOperators() As String()
-        Return {"AND", "OR"}
-    End Function
-
-End Class
---
+++ WarehouseReports/VM/TreeVM/Enums/StringEnums.vb
@@ -0,0 +1,21 @@
+﻿Public Class StringEnums
+
+    Public Function GetExpressionObjects() As Dictionary(Of String, String)
+        Return New Dictionary(Of String, String) From {
+            {"Склад отправитель", "F6"},
+            {"Адрес отправитель", "F7"},
+            {"Склад получатель", "F8"},
+            {"Адрес получатель", "F9"}}
+    End Function
+
+
+    Public Function GetExpressionOperators() As String()
+        Return {"IN", "NOT IN", "IS NULL", "IS NOT NULL"}
+    End Function
+
+
+    Public Function GetLogicOperators() As String()
+        Return {"AND", "OR"}
+    End Function
+
+End Class
